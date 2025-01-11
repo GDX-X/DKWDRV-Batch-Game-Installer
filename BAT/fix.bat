ae47fafd870f8b31423d0d8552f04fb0d31358bf
GDX-X <GDX2ATV@hotmail.fr>
2025-01-11 12:17:10 +0100

    Add files via upload

--- BAT/fix.bat
+++ BAT/fix.bat
@@ -4,6 +4,6 @@ cd ..
 set rootpath=%cd%
 
 REM PS1
-"%rootpath%\BAT\Busybox" sed -i "2,$s/!/^!/g; 2,$s/ :/:/g; 2,$s/ :/:/g; 2,$s/:/ -/g; 2,$s/?//g; 2,$s/*//g; 2,$s/\///g" "%rootpath%\BAT\TitlesDB_PS1_English.txt"
+"%rootpath%\BAT\Busybox" sed -i "2,$s/!/^!/g; 2,$s/ :/:/g; 2,$s/ :/:/g; 2,$s/:/ -/g; 2,$s/?//g; 2,$s/*//g; 2,$s/\//-/g" "%rootpath%\BAT\TitlesDB_PS1_English.txt"
 
 pause
